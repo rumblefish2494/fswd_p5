62652af9884641c4aa66520f1bdc57d2b5cea8b0
rumblefish2494 <rumbelfish2494@gmail.com>
2015-11-24 21:43:43 -0800

    all decorators and routes working

--- muze.py
+++ muze.py
@@ -31,6 +31,7 @@ def newArtist():
 	output += '</h1>'
 	return output
 
+#edit artist
 @app.route('/artists/<int:artist_id>/edit/')
 def editArtist(artist_id):
 	output = ""
@@ -39,6 +40,14 @@ def editArtist(artist_id):
 	output += '</h1>'
 	return output
 
+#delete artist
+@app.route('/artists/<int:artist_id>/delete/')
+def deleteArtist(artist_id):
+	output = ""
+	output += '<h1>'
+	output += 'DELETE ARTIST'
+	output += '</h1>'
+	return output
 
 #show list of albums for artist
 @app.route('/artists/<int:artist_id>/albums/')
@@ -49,12 +58,66 @@ def showAlbums(artist_id):
 	output += '</h1>'
 	return output
 
+#add new album
+@app.route('/artists/<int:artist_id>/albums/new/')
+def newAlbum(artist_id):
+	output = ""
+	output += '<h1>'
+	output += 'ADD ALBUM'
+	output += '</h1>'
+	return output
+
+#edit album
+@app.route('/artists/<int:artist_id>/albums/<int:album_id>/edit/')
+def editAlbum(artist_id, album_id):
+	output = ""
+	output += '<h1>'
+	output += 'EDIT ALBUM'
+	output += '</h1>'
+	return output
+
+#delete album
+@app.route('/artists/<int:artist_id>/albums/<int:album_id>/delete/')
+def deleteAlbum(artist_id, album_id):
+	output = ""
+	output += '<h1>'
+	output += 'DELETE ALBUM'
+	output += '</h1>'
+	return output
+
 #show list of songs for artist/album
 @app.route('/artists/<int:artist_id>/albums/<int:album_id>/songs/')
 def showSongs(artist_id, album_id):
 	output = ""
 	output += '<h1>'
 	output += 'SONG LIST'
+	output += '</h1>'
+	return output
+
+#add new song
+@app.route('/artists/<int:artist_id>/albums/<int:album_id>/songs/new/')
+def newSong(artist_id, album_id):
+	output = ""
+	output += '<h1>'
+	output += 'ADD SONG'
+	output += '</h1>'
+	return output
+
+#edit song
+@app.route('/artists/<int:artist_id>/albums/<int:album_id>/songs/<int:song_id>/edit/')
+def editSongs(artist_id, album_id, song_id):
+	output = ""
+	output += '<h1>'
+	output += 'EDIT SONG'
+	output += '</h1>'
+	return output
+
+#delete song
+@app.route('/artists/<int:artist_id>/albums/<int:album_id>/songs/<int:song_id>/delete/')
+def deleteSong(artist_id, album_id, song_id):
+	output = ""
+	output += '<h1>'
+	output += 'DELETE SONG'
 	output += '</h1>'
 	return output
 
